56b1fb51933c59da87e10377588445f640268400
M. Imam Pratama <imambungo@gmail.com>
2018-10-11 06:37:07 +0700

    add enqueue method

--- CircularArrayQueue.java
+++ CircularArrayQueue.java
@@ -13,4 +13,25 @@ public class CircularArrayQueue {
         this.rear = 0;
         this.array = new int[this.size];
     }
+
+    public CircularArrayQueue(int size) {
+        this.size = size;
+        this.front = -1;
+        this.rear = 0;
+        this.array = new int[this.size];
+    }
+
+    public boolean isFull() {
+        return this.size == this.array.length;
+    }
+
+    public void enqueue(int value) {
+        if (isFull()) {
+            System.out.println("Gak muat lagi gan ¯\\_(ツ)_/¯");
+        } else {
+            this.rear = ++this.rear % this.array.length;
+            this.array[this.rear] = value;
+            this.size++;
+        }
+    }
 }
